e3c1bc708e620d81762001cf6529bf2347f292b5
haku1806 <thangtranhtn2000@gmail.com>
2021-10-03 22:04:16 +0700

    update database

--- LHUDating.sql
+++ LHUDating.sql
@@ -12,7 +12,38 @@ CREATE TABLE [dbo].[User](
 	[LastLogin] [datetime] NULL,
 	[CurrentSession] [varchar](500) NULL,
  CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
-(
-	[Code] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+)
+
+CREATE TABLE [dbo].[Contact](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserCode] [varchar](32) NOT NULL,
+	[UserCode2] [varchar](32) NOT NULL,
+	[isMatch] [boolean] NOT NULL,
+	[Created] [datetime] NOT NULL
+)
+
+CREATE TABLE [dbo].[Group](
+	[Code] [varchar](32) NOT NULL,
+	[Type] [varchar](32) NOT NULL,
+	[Avatar] [varchar](max) NULL,
+	[Name] [nvarchar](250) NULL,
+	[Created] [datetime] NOT NULL,
+	[CreatedBy] [varchar](32) NOT NULL,
+	[LastActive] [datetime] NOT NULL
+)
+
+CREATE TABLE [dbo].[GroupUser](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[GroupCode] [varchar](32) NOT NULL,
+	[UserCode] [varchar](32) NOT NULL
+)
+
+CREATE TABLE [dbo].[Message](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Type] [varchar](10) NOT NULL,
+	[GroupCode] [varchar](32) NOT NULL,
+	[Content] [nvarchar](max) NULL,
+	[Path] [nvarchar](255) NULL,
+	[Created] [datetime] NOT NULL,
+	[CreatedBy] [varchar](32) NOT NULL,
+)
